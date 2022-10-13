c3b8bad1b9069d60240997b290228969c96ce099
Dongdong Ke <d.ke@student.fontys.nl>
2022-10-13 23:25:22 +0200

    user input

---
+++ Application/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library;
+
+namespace Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fugro. Enjoy your assignment!");
+            List<Position> positions = new List<Position>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Input No.{i+1} latitude");
+                double latitude = UserInput();
+                Console.WriteLine($"Input No.{i+1} longtitude");
+                double longtitude = UserInput();
+                Console.WriteLine($"Input No.{i+1} height");
+                double height = UserInput();
+                positions.Add(new Position(latitude, longtitude, height));
+            }
+
+        }
+        public static double UserInput()
+        {
+            double value = -1;
+            try
+            {
+                string input = Console.ReadLine();
+                value = double.Parse(input);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Wrong data, input again. Msg:" + e.Message);
+                return UserInput();
+            }
+            return value;
+        }
+
+
+
+    }
+}
