f3c28990c143dcb00b8c424151a061cc6bd4bf17
Dongdong Ke <d.ke@student.fontys.nl>
2022-10-14 01:11:28 +0200

    Add PositionDuplicatedException, validate the range of latitude and longitude

--- Application/Program.cs
+++ Application/Program.cs
@@ -15,13 +15,23 @@ namespace Application
             {
                 Console.WriteLine($"Input No.{i+1} latitude");
                 double latitude = UserInput();
+                if (latitude<-90 || latitude>90)
+                {
+                    Console.WriteLine("Error! Please input latitude between -90 and 90 degree");
+                    latitude = UserInput();
+                }
                 Console.WriteLine($"Input No.{i+1} longtitude");
-                double longtitude = UserInput();
+                double longitude = UserInput();
+                if (longitude < -180 || longitude > 180)
+                {
+                    Console.WriteLine("Error! Please input longtitude between -180 and 180 degree");
+                    longitude = UserInput();
+                }
                 Console.WriteLine($"Input No.{i+1} height");
                 double height = UserInput();
-                positions.Add(new Position(latitude, longtitude, height));
+                positions.Add(new Position(latitude, longitude, height));
             }
-
+            checkDuplicatedPosition(positions);
         }
         public static double UserInput()
         {
@@ -30,7 +40,6 @@ namespace Application
             {
                 string input = Console.ReadLine();
                 value = double.Parse(input);
-
             }
             catch (Exception e)
             {
@@ -40,7 +49,22 @@ namespace Application
             return value;
         }
 
+        public static void checkDuplicatedPosition(List<Position> positionsList)
+        {
+            var list =  positionsList.GroupBy(x => new { x.Longitude, x.Latitude, x.Height }).ToList();
+            foreach (var i in list)
+            {
+                if (i.Count() > 1)
+                    throw new PositionDuplicatedException("Position Duplicated");
+            }
+        }
 
-
+        public static void WriteToConsole(List<Position> items)
+        {
+            foreach (object o in items)
+            {
+                Console.WriteLine(o);
+            }
+        }
     }
 }
